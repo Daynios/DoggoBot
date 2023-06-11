1d90f646dc7d3ed7e91198542f28618acf5f6710
Daynios <daynios0@gmail.com>
2023-06-11 17:15:14 +0100

    Update guildCreate.js

--- Events/guildCreate.js
+++ Events/guildCreate.js
@@ -16,7 +16,7 @@ module.exports =  async (client, guild) => {
     .addField('Name', `\`${guild.name}\``)
     .addField('ID', `\`${guild.id}\``)
     .addField('Member Count', `\`${guild.memberCount}\` Members`)
-    .addField('Owner', `\`${guild.owner.user.tag}\` | \`${guild.owner.id}\``)
+    // .addField('Owner', `\`${guild.owner.user.tag}\` | \`${guild.owner.id}\``)
     .addField('Creation Date', `\`${moment.utc(guild.createdAt).format('DD/MMM/YYYY')}\``)
     .addField('Region', `\`${guild.region}\``)
     .addField(`${client.user.username}'s Server Count`, `\`${client.guilds.cache.size}\` Severs`)
